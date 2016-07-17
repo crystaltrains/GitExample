2c257e7c362d4468ee600bb4732ceaed27ca6599
Crystal THOMPSON <Crystal_T@crystals-macbook-pro.local>
2016-07-17 16:28:41 -0400

    right place

--- Existing Project/ViewController.swift
+++ Existing Project/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        // Extra Comment
+        // Extra Comment Another Comment
     }
 
     override func didReceiveMemoryWarning() {
